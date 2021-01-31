6598834f79e6ec6965305edec78685445dbe9596
Colin McCullough <cmccullough@macg.com>
2021-01-30 22:07:14 -0500

    updating site
    Signed-off-by: Colin McCullough <cmccullough@macg.com>

--- src/lambda/fetchWishes.js
+++ src/lambda/fetchWishes.js
@@ -3,17 +3,22 @@ const axios = require('axios');
 
 export async function handler(event, context) {
     var token = process.env.netlify_access_token;
+    var siteId = process.env.SITE_ID;
 
     const baseUrl = "https://api.netlify.com"
     var queryToken = `access_token=${token}`;
-    console.log(queryToken)
 
-    const sitesResults =  await axios.get(`${baseUrl}/api/v1/sites/${process.env.site_id}/forms?${queryToken}`);
+    const sitesResult =  await axios.get(`${baseUrl}/api/v1/sites/${siteId}/forms?${queryToken}`);
 
     console.log(sitesResult)
 
-    const form = sitesResult?.data?.filter(x => x.name == "birthday")
+    const form = sitesResult?.data?.filter(x => x.name == "birthday")[0]
 
-    return  axios.get(`${baseUrl}/api/v1/forms/${form.id}/submissions?${queryToken}`)
+    const result = await  axios.get(`${baseUrl}/api/v1/forms/${form.id}/submissions?${queryToken}`)
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(result.data?.map(x => x.data))
+    }
 
 }
